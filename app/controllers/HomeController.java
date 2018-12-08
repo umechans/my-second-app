be656a13a1fc6333fac9fb66c6b361672bcedce9
岡本恵太 <okamotokeita@okamotokeitanoMacBook-puro.local>
2018-12-08 10:43:42 +0900

    no message

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,8 +1,8 @@
 package controllers;
 
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -17,6 +17,7 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
+
         return ok(index.render("Your new application is ready."));
     }
 
