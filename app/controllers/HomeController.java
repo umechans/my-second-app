9dfe4174c4a56002416b88a337c1babe91418b33
岡本恵太 <okamotokeita@okamotokeitanoMacBook-puro.local>
2018-12-16 12:58:14 +0900

    formの作成

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,23 +1,30 @@
 package controllers;
+import static play.data.Form.*;
 
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
-/**
- * This controller contains an action to handle HTTP requests
- * to the application's home page.
- */
 public class HomeController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
-    public Result index() {
-        return ok(index.render("ピラルク","ピラニア","カンディル"));
-    }
+		public static class SampleForm{
+			public String message;
+		}
 
+	    public Result index() {
+	    	Form<SampleForm> f1 = form(SampleForm.class);
+	        return ok(index.render("何か書いて。", f1));
+	    }
+
+	    public Result send() {
+	    	Form<SampleForm> f2 =  form(SampleForm.class).bindFromRequest();
+	    	if (!f2.hasErrors()) {
+	    	SampleForm data = f2.get();
+	    	String msg = "you typed:" + data.message;
+	    	return ok(index.render(msg,f2));
+	    }else{
+	    	return badRequest(index.render("ERROR", form(SampleForm.class)));
+	    }
+	}
 }
